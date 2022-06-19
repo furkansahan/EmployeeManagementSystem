c95fc9bd7741dff793aae61d0a4c7e809e973a51
Furkan Sahan <mustafafurkansahan@gmail.com>
2022-06-19 21:35:35 +0300

    update,delete,page settings

--- src/main/java/com/furkansahan/employeemanagementsystem/controller/EmpController.java
+++ src/main/java/com/furkansahan/employeemanagementsystem/controller/EmpController.java
@@ -3,10 +3,12 @@ package com.furkansahan.employeemanagementsystem.controller;
 import com.furkansahan.employeemanagementsystem.entity.Employee;
 import com.furkansahan.employeemanagementsystem.service.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
@@ -17,22 +19,54 @@ public class EmpController {
     @Autowired
     private EmpService service;
 
-
     @GetMapping("/")
-    public String home(Model model){
-        List<Employee> emp=service.getAllEmp();
-        model.addAttribute("emp");
-        return "index";
+    public String home(Model m) {
+        return findPaginated(0, m);
     }
+
     @GetMapping("/addemp")
-    public String addEmp(){
-        return"add_emp";
+    public String addEmpForm() {
+        return "add_emp";
     }
+
     @PostMapping("/register")
-    public String empRegister(@ModelAttribute Employee e , HttpSession session){
-        System.out.println(e);
+    public String empRegister(@ModelAttribute Employee e, HttpSession session) {
         service.addEmp(e);
-        session.setAttribute("msg","Employee Added Successfully");
+        session.setAttribute("msg", "Emplyoee Added Sucessfully..");
         return "redirect:/";
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model m) {
+        Employee e = service.getEmpById(id);
+        m.addAttribute("emp", e);
+        return "edit";
+    }
+
+    @PostMapping("/update")
+    public String updateEmp(@ModelAttribute Employee e, HttpSession session) {
+        service.addEmp(e);
+        session.setAttribute("msg", "Employee Data Update Sucessfully..");
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteEmp(@PathVariable int id, HttpSession session) {
+
+        service.deleteEmp(id);
+        session.setAttribute("msg", "Employee Data Delete Sucessfully..");
+        return "redirect:/";
+    }
+
+    @GetMapping("/page/{pageno}")
+    public String findPaginated(@PathVariable int pageno, Model m) {
+
+        Page<Employee> emplist = service.getEmpByPaginate(pageno, 10);
+        m.addAttribute("emp", emplist);
+        m.addAttribute("currentPage", pageno);
+        m.addAttribute("totalPages", emplist.getTotalPages());
+        m.addAttribute("totalItem", emplist.getTotalElements());
+        return "index";
+    }
+
 }
--- src/main/java/com/furkansahan/employeemanagementsystem/service/EmpService.java
+++ src/main/java/com/furkansahan/employeemanagementsystem/service/EmpService.java
@@ -3,20 +3,43 @@ package com.furkansahan.employeemanagementsystem.service;
 import com.furkansahan.employeemanagementsystem.entity.Employee;
 import com.furkansahan.employeemanagementsystem.repository.EmpRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmpService {
 
     @Autowired
-    private EmpRepository repository;
+    private EmpRepository repo;
 
-    public void addEmp(Employee e){
-        repository.save(e);
+    public void addEmp(Employee e) {
+        repo.save(e);
     }
-    public List<Employee> getAllEmp(){
-        return repository.findAll();
+
+    public List<Employee> getAllEmp() {
+        return repo.findAll();
     }
+
+    public Employee getEmpById(int id) {
+        Optional<Employee> e = repo.findById(id);
+        if (e.isPresent()) {
+            return e.get();
+        }
+        return null;
+    }
+
+    public void deleteEmp(int id) {
+        repo.deleteById(id);
+    }
+
+    public Page<Employee> getEmpByPaginate(int currentPage, int size) {
+        Pageable p = PageRequest.of(currentPage, size);
+        return repo.findAll(p);
+    }
+
 }
